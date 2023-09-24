0a5bf8ed08b5e2465d910c924cf26bad4a898934
tetrapod418 <hikeybow@gmail.com>
2023-09-24 19:26:16 +0900

    first commot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const kintoneRecords = 'http://localhost:5000/getData';
+
+const callRestApi = async () => {
+  const response = await fetch(kintoneRecords);
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
+  //return JSON.stringify(jsonResponse);
+  const arrayOfLists = jsonResponse.records.map(
+    record => <dl><dt>{record.title.value}</dt><dd className="ddimg"><img src={getQRCodeUrl(record.URL.value)} alt={record.URL.value} title={record.title.value} /> </dd><dd className="descriptions">{record.descriptions.value}</dd></dl>
+    )
+    return arrayOfLists;
+};
+
+function getQRCodeUrl(url) {
+  return `http://api.qrserver.com/v1/create-qr-code/?data=${url}&size=100x100`;
 }
+function App() {
+  const [apiResponse, setApiResponse] =
+  useState('*** now loading ***');
+  useEffect(() => {
+    callRestApi().then(
+      result => setApiResponse(result));
+  },[]);
+  return(
+    <div>
+      <h1>kintoneアプリから取得したURLのリスト</h1>
+      <div className="container">{apiResponse}</div>
+  </div>
+  );
+  };
 
 export default App;
